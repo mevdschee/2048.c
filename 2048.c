de867a2f62fd1c729462e600db815d7ff213f1ba
Maurits van der Schee <m.vanderschee@leaseweb.com>
2014-03-29 00:31:13 +0100

    Changed width of variable

--- 2048.c
+++ 2048.c
@@ -269,8 +269,9 @@ int test() {
 		8,0,2,2,	8,4,0,0,
 		4,0,2,2,	4,4,0,0
 	};
-	uint16_t *in,*out,tests;
-	uint8_t i,t;
+	uint16_t *in,*out;
+	uint16_t t,tests;
+	uint8_t i;
 	bool success = true;
 
 	tests = (sizeof(data)/sizeof(data[0]))/(2*SIZE);
