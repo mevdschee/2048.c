d53b9200daed10b365e85f2eb6ebff3256e55da6
lovasoa <pere.jobs@gmail.com>
2014-04-01 04:21:14 +0200

    Slide the board in any direction without rotating
    Add a function: slide(board, direction)

--- 2048.c
+++ 2048.c
@@ -18,17 +18,25 @@
 #include <signal.h>
 
 #define SIZE 4
+
+#define COLORSET_ORIGINAL {8,255,1,255,2,255,3,255,4,255,5,255,6,255,7,255,9,0,10,0,11,0,12,0,13,0,14,0,255,0,255,0}
+#define COLORSET_BLACKWHITE {232,255,234,255,236,255,238,255,240,255,242,255,244,255,246,0,248,0,249,0,250,0,251,0,252,0,253,0,254,0,255,0}
+#define COLORSET_BLUERED {235,255,63,255,57,255,93,255,129,255,165,255,201,255,200,255,199,255,198,255,197,255,196,255,196,255,196,255,196,255,196,255}
+
+#define CELL(i) *(getcell(board, dir, row, i))
+typedef uint16_t t_cell;
+typedef t_cell t_row[SIZE];
+typedef t_row t_board[SIZE];
+
 uint32_t score=0;
 
 void getColor(uint16_t value, char *color, size_t length) {
-	uint8_t original[] = {8,255,1,255,2,255,3,255,4,255,5,255,6,255,7,255,9,0,10,0,11,0,12,0,13,0,14,0,255,0,255,0};
-	uint8_t blackwhite[] = {232,255,234,255,236,255,238,255,240,255,242,255,244,255,246,0,248,0,249,0,250,0,251,0,252,0,253,0,254,0,255,0};
-	uint8_t bluered[] = {235,255,63,255,57,255,93,255,129,255,165,255,201,255,200,255,199,255,198,255,197,255,196,255,196,255,196,255,196,255,196,255};
-	uint8_t *scheme = original;
+	uint8_t colorset[] = COLORSET_ORIGINAL;
+	uint8_t *scheme = colorset;
 	uint8_t *background = scheme+0;
 	uint8_t *foreground = scheme+1;
 	if (value > 0) while (value >>= 1) {
-		if (background+2<scheme+sizeof(original)) {
+		if (background+2<scheme+sizeof(colorset)) {
 			background+=2;
 			foreground+=2;
 		}
@@ -78,115 +86,62 @@ void drawBoard(uint16_t board[SIZE][SIZE]) {
 	printf("\033[A");
 }
 
-int8_t findTarget(uint16_t array[SIZE],int8_t x,int8_t stop) {
-	int8_t t;
-	// if the position is already on the first, don't evaluate
-	if (x==0) {
-		return x;
+t_cell* getcell (t_board board, char dir, int numrow, int index) {
+	dir = dir%4;
+	if (dir==1 || dir==2) index = SIZE-1-index;
+	if (dir >= 2) numrow = SIZE-1-numrow;
+	if (dir%2 == 1) {
+		int tmp = numrow;
+		numrow = index;
+		index = tmp;
 	}
-	for(t=x-1;t>=0;t--) {
-		if (array[t]!=0) {
-			if (array[t]!=array[x]) {
-				// merge is not possible, take next position
-				return t+1;
+	if (numrow<0 || numrow>SIZE || index < 0 || index>SIZE) return NULL;
+	return &(board[numrow][index]);
+}
+
+char slide (t_board board, char dir) {
+	char success = 0;
+	int row;
+	for (row=0; row < SIZE; row++) {
+		int cur=0,next=0,write=0;
+		char rowsuccess = 0;
+		while (cur < SIZE && CELL(cur) == 0) {
+			cur++; next++;
+		}
+		if (cur>0 && cur<SIZE) rowsuccess=1;
+		while (cur < SIZE) {
+			do {next++;} while ( next < SIZE && CELL(next) == 0);
+			if (next < SIZE && next != cur+1) rowsuccess=1;
+			if (next < SIZE && CELL(cur) == CELL(next)) {
+				rowsuccess = 1;
+				CELL(cur) *= 2;
+				score += CELL(cur);
+				next++;
 			}
-			return t;
-		} else {
-			// we should not slide further, return this one
-			if (t==stop) {
-				return t;
-			}
+			CELL(write) = CELL(cur);
+			write++;
+			cur = next;
+		}
+		if (!rowsuccess) continue;
+		while (write<SIZE) {
+			CELL(write) = 0;
+			write++;
+		}
+		success |= rowsuccess;
+	}
+	return success;
+}
+
+
+bool canMoveInDir (t_board board, char dir) {
+	int row,i;
+	for (row=0;row<SIZE;row++) {
+		for (i=0;i<SIZE-1;i++) {
+			t_cell a=CELL(i), b=CELL(i+1);
+			if (a==0 || b == 0 || a == b) return true;
 		}
 	}
-	// we did not find a
-	return x;
-}
-
-bool slideArray(uint16_t array[SIZE]) {
-	bool success = false;
-	int8_t x,t,stop=0;
-
-	for (x=0;x<SIZE;x++) {
-		if (array[x]!=0) {
-			t = findTarget(array,x,stop);
-			// if target is not original position, then move or merge
-			if (t!=x) {
-				// if target is not zero, set stop to avoid double merge
-				if (array[t]!=0) {
-					score+=array[t]+array[x];
-					stop = t+1;
-				}
-				array[t]+=array[x];
-				array[x]=0;
-				success = true;
-			}
-		}
-	}
-	return success;
-}
-
-void rotateBoard(uint16_t board[SIZE][SIZE]) {
-	int8_t i,j,n=SIZE;
-	uint16_t tmp;
-	for (i=0; i<n/2; i++){
-		for (j=i; j<n-i-1; j++){
-			tmp = board[i][j];
-			board[i][j] = board[j][n-i-1];
-			board[j][n-i-1] = board[n-i-1][n-j-1];
-			board[n-i-1][n-j-1] = board[n-j-1][i];
-			board[n-j-1][i] = tmp;
-		}
-	}
-}
-
-bool moveUp(uint16_t board[SIZE][SIZE]) {
-	bool success = false;
-	int8_t x;
-	for (x=0;x<SIZE;x++) {
-		success |= slideArray(board[x]);
-	}
-	return success;
-}
-
-bool moveLeft(uint16_t board[SIZE][SIZE]) {
-	bool success;
-	rotateBoard(board);
-	success = moveUp(board);
-	rotateBoard(board);
-	rotateBoard(board);
-	rotateBoard(board);
-	return success;
-}
-
-bool moveDown(uint16_t board[SIZE][SIZE]) {
-	bool success;
-	rotateBoard(board);
-	rotateBoard(board);
-	success = moveUp(board);
-	rotateBoard(board);
-	rotateBoard(board);
-	return success;
-}
-
-bool moveRight(uint16_t board[SIZE][SIZE]) {
-	bool success;
-	rotateBoard(board);
-	rotateBoard(board);
-	rotateBoard(board);
-	success = moveUp(board);
-	rotateBoard(board);
-	return success;
-}
-
-bool findPairDown(uint16_t board[SIZE][SIZE]) {
-	bool success = false;
-	int8_t x,y;
-	for (x=0;x<SIZE;x++) {
-		for (y=0;y<SIZE-1;y++) {
-			if (board[x][y]==board[x][y+1]) return true;
-		}
-	}
-	return success;
+	return false;
 }
 
 int16_t countEmpty(uint16_t board[SIZE][SIZE]) {
@@ -203,15 +158,7 @@ int16_t countEmpty(uint16_t board[SIZE][SIZE]) {
 }
 
 bool gameEnded(uint16_t board[SIZE][SIZE]) {
-	bool ended = true;
-	if (countEmpty(board)>0) return false;
-	if (findPairDown(board)) return false;
-	rotateBoard(board);
-	if (findPairDown(board)) ended = false;
-	rotateBoard(board);
-	rotateBoard(board);
-	rotateBoard(board);
-	return ended;
+	return ( !canMoveInDir(board,0) && !canMoveInDir(board,1));
 }
 
 void addRandom(uint16_t board[SIZE][SIZE]) {
@@ -268,67 +215,6 @@ void setBufferedInput(bool enable) {
 	}
 }
 
-int test() {
-	uint16_t array[SIZE];
-	uint16_t data[] = {
-		0,0,0,2,	2,0,0,0,
-		0,0,2,2,	4,0,0,0,
-		0,2,0,2,	4,0,0,0,
-		2,0,0,2,	4,0,0,0,
-		2,0,2,0,	4,0,0,0,
-		2,2,2,0,	4,2,0,0,
-		2,0,2,2,	4,2,0,0,
-		2,2,0,2,	4,2,0,0,
-		2,2,2,2,	4,4,0,0,
-		4,4,2,2,	8,4,0,0,
-		2,2,4,4,	4,8,0,0,
-		8,0,2,2,	8,4,0,0,
-		4,0,2,2,	4,4,0,0
-	};
-	uint16_t *in,*out;
-	uint16_t t,tests;
-	uint8_t i;
-	bool success = true;
-
-	tests = (sizeof(data)/sizeof(data[0]))/(2*SIZE);
-	for (t=0;t<tests;t++) {
-		in = data+t*2*SIZE;
-		out = in + SIZE;
-		for (i=0;i<SIZE;i++) {
-			array[i] = in[i];
-		}
-		slideArray(array);
-		for (i=0;i<SIZE;i++) {
-			if (array[i] != out[i]) {
-				success = false;
-			}
-		}
-		if (success==false) {
-			for (i=0;i<SIZE;i++) {
-				printf("%d ",in[i]);
-			}
-			printf("=> ");
-			for (i=0;i<SIZE;i++) {
-				printf("%d ",array[i]);
-			}
-			printf("expected ");
-			for (i=0;i<SIZE;i++) {
-				printf("%d ",in[i]);
-			}
-			printf("=> ");
-			for (i=0;i<SIZE;i++) {
-				printf("%d ",out[i]);
-			}
-			printf("\n");
-			break;
-		}
-	}
-	if (success) {
-		printf("All %u tests executed successfully\n",tests);
-	}
-	return !success;
-}
-
 void signal_callback_handler(int signum) {
 	printf("         TERMINATED         \n");
 	setBufferedInput(true);
@@ -337,13 +223,9 @@ void signal_callback_handler(int signum) {
 }
 
 int main(int argc, char *argv[]) {
-	uint16_t board[SIZE][SIZE];
+	t_board board;
 	char c;
 	bool success;
-
-	if (argc == 2 && strcmp(argv[1],"test")==0) {
-		return test();
-	}
 
 	printf("\033[?25l\033[2J\033[H");
 
@@ -359,15 +241,16 @@ int main(int argc, char *argv[]) {
 		c=getchar();
 		switch(c) {
 			case 68:	// left arrow
-				success = moveLeft(board);  break; 
+				success = slide(board,3);  break; 
 			case 67:	// right arrow
-				success = moveRight(board); break; 
+				success = slide(board,1); break; 
 			case 65:	// up arrow
-				success = moveUp(board);    break; 
+				success = slide(board,0);    break; 
 			case 66:	// down arrow
-				success = moveDown(board);  break; 
+				success = slide(board,2);  break; 
 			default: success = false;
 		}
+
 		if (success) {
 			drawBoard(board);
 			usleep(150000);
@@ -389,4 +272,3 @@ int main(int argc, char *argv[]) {
 
 	return EXIT_SUCCESS;
 }
-
