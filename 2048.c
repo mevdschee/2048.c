5f11bcbef31337e0fdd46b02a821ba269f2e7859
Farooq Karimi Zadeh <farooghkarimizadeh@gmail.com>
2018-03-24 19:54:00 +0430

    Update 2048.c

--- 2048.c
+++ 2048.c
@@ -413,7 +413,7 @@ int main(int argc, char *argv[]) {
 			default: success = false;
 		}
 		if (success) {
-			// checking if Terminal height as width was changed
+			// checking if Terminal height and width was changed
 			ioctl(STDOUT_FILENO, TIOCGWINSZ, &TerminalSize_new);
 			if (TerminalSize.ws_row != TerminalSize_new.ws_row || TerminalSize.ws_col != TerminalSize_new.ws_col){
 				printf("\033[2J");
