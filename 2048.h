ee6289c46f302117482329ea8a3c3972ecf26197
Egorow Ilya <Fanzen2010@gmail.com>
2019-02-24 20:16:47 +0300

    Added common header file.

---
+++ 2048.h
@@ -0,0 +1,28 @@
+#ifndef H2048H
+#define H2048H
+/*
+ ============================================================================
+ Name        : 2048.h
+ Author      : Ilya Egorov
+ Description : Header File for 2048.c
+ ============================================================================
+ */
+#define SIZE 4
+void signal_callback_handler(int signum) ;
+int test();
+void setBufferedInput(bool enable);
+void initBoard(uint8_t board[SIZE][SIZE]);
+void addRandom(uint8_t board[SIZE][SIZE]);
+bool gameEnded(uint8_t board[SIZE][SIZE]);
+uint8_t countEmpty(uint8_t board[SIZE][SIZE]);
+bool findPairDown(uint8_t board[SIZE][SIZE]);
+bool moveUp(uint8_t board[SIZE][SIZE]);
+bool moveLeft(uint8_t board[SIZE][SIZE]);
+bool moveDown(uint8_t board[SIZE][SIZE]);
+bool moveRight(uint8_t board[SIZE][SIZE]);
+void rotateBoard(uint8_t board[SIZE][SIZE]);
+bool slideArray(uint8_t array[SIZE]);
+uint8_t findTarget(uint8_t array[SIZE],uint8_t x,uint8_t stop);
+void drawBoard(uint8_t board[SIZE][SIZE]);
+void getColor(uint8_t value, char *color, size_t length);
+#endif /* end of include guard: 2048.H */
