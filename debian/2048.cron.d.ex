4122dc403fe168d145d1e75df960e11e20f11433
Limows DigitalDemon <limowski256@gmail.com>
2021-04-22 01:14:46 -0500

    clean repo

--- debian/2048.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the 2048 package
-#
-0 4	* * *	root	[ -x /usr/bin/2048_maintenance ] && /usr/bin/2048_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(2048):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="2048" command="/usr/bin/2048"
