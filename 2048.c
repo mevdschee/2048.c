64f0448fb370300a893ee45f988b3f447ccd98ea
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-10-20 14:18:16 -0400

    `const`

--- 2048.c
+++ 2048.c
@@ -27,22 +27,22 @@ uint32_t score = 0;
 __attribute__((pure)) uint8_t *getColors(uint8_t value, const uint8_t scheme)
 {
 	static uint8_t out[2] = {0, 0};
-	uint8_t original[] = {8, 255, 1, 255, 2, 255, 3, 255, 4, 255, 5, 255, 6, 255, 7, 255, 9, 0, 10, 0, 11, 0, 12, 0, 13, 0, 14, 0, 255, 0, 255, 0};
-	uint8_t blackwhite[] = {232, 255, 234, 255, 236, 255, 238, 255, 240, 255, 242, 255, 244, 255, 246, 0, 248, 0, 249, 0, 250, 0, 251, 0, 252, 0, 253, 0, 254, 0, 255, 0};
-	uint8_t bluered[] = {235, 255, 63, 255, 57, 255, 93, 255, 129, 255, 165, 255, 201, 255, 200, 255, 199, 255, 198, 255, 197, 255, 196, 255, 196, 255, 196, 255, 196, 255, 196, 255};
-	uint8_t *schemes[] = {original, blackwhite, bluered};
-	uint8_t *s = schemes[scheme];
+	const uint8_t original[] = {8, 255, 1, 255, 2, 255, 3, 255, 4, 255, 5, 255, 6, 255, 7, 255, 9, 0, 10, 0, 11, 0, 12, 0, 13, 0, 14, 0, 255, 0, 255, 0};
+	const uint8_t blackwhite[] = {232, 255, 234, 255, 236, 255, 238, 255, 240, 255, 242, 255, 244, 255, 246, 0, 248, 0, 249, 0, 250, 0, 251, 0, 252, 0, 253, 0, 254, 0, 255, 0};
+	const uint8_t bluered[] = {235, 255, 63, 255, 57, 255, 93, 255, 129, 255, 165, 255, 201, 255, 200, 255, 199, 255, 198, 255, 197, 255, 196, 255, 196, 255, 196, 255, 196, 255, 196, 255};
+	const uint8_t *schemes[] = {original, blackwhite, bluered};
+	const uint8_t *s = schemes[scheme];
 	value *= 2;
-	unsigned long len = sizeof(original);
-	uint8_t fg = *(s + (1 + value) % len);
-	uint8_t bg = *(s + (0 + value) % len);
+	const unsigned long len = sizeof(original);
+	const uint8_t fg = *(s + (1 + value) % len);
+	const uint8_t bg = *(s + (0 + value) % len);
 	out[0] = fg;
 	out[1] = bg;
 	return out;
 }
 
 /** calculates `floor(log_10(n) + 1)` */
-uint8_t digitCount(uint32_t n)
+__attribute__((pure)) uint8_t digitCount(uint32_t n)
 {
 	uint8_t c = 0;
 	do
@@ -53,7 +53,7 @@ uint8_t digitCount(uint32_t n)
 	return c;
 }
 
-void drawBoard(uint8_t board[SIZE][SIZE], uint8_t scheme)
+void drawBoard(const uint8_t board[SIZE][SIZE], const uint8_t scheme)
 {
 	uint8_t x, y, fg, bg;
 	printf("\033[H");
